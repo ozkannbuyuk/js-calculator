2175a12fb4b2e7d695e103a0e7fcd762d853283b
ozkanbuyuk <ozkanbuyuk@icloud.com>
2023-12-25 23:24:12 +0300

    JavaScript - Calculator

---
+++ script.js
@@ -0,0 +1,47 @@
+var screen = document.querySelector("#screen");
+var btnNumber = document.querySelectorAll("#btnNumber");
+var btnOpt = document.querySelectorAll("#btnOpt");
+var optState = false;
+var opt = "";
+var final = 0;
+
+btnNumber.forEach((element) => {
+  element.onclick = function () {
+    this.style.fontSize = "25px";
+    if (screen.textContent == "0" || optState) {
+      screen.textContent = "";
+    }
+    screen.textContent += this.textContent;
+    optState = false;
+    setTimeout(() => {
+      this.style.fontSize = "16px";
+    }, 500);
+  };
+});
+
+btnOpt.forEach((element) => {
+  element.onclick = function () {
+    optState = true;
+    var newOpt = this.textContent;
+
+    switch (opt) {
+      case "+":
+        screen.textContent = final + Number(screen.textContent);
+        break;
+      case "-":
+        screen.textContent = final - Number(screen.textContent);
+        break;
+      case "*":
+        screen.textContent = final * Number(screen.textContent);
+        break;
+      case "/":
+        screen.textContent = final / Number(screen.textContent);
+        break;
+      case "=":
+        final = Number(screen.textContent);
+        break;
+    }
+    final = Number(screen.textContent);
+    opt = newOpt;
+  };
+});
